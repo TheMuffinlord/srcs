d3ca8bb44bbeebbbf34a1637aacceb8b4a313f43
themuffinlord <tcgiant@gmail.com>
2025-05-05 17:30:01 -0400

    ok so instead of rebasing i built a quick camera system as a proof

--- battle_loop.py
+++ battle_loop.py
@@ -9,13 +9,29 @@ from equipment import BasicBullet, BasicLaser, Particle
 from textboxes import *
 
 def kbd_interpreter(pressed_key):
-    global PlayerGroup
+    global PlayerGroup, offset_y, offset_x, screenmax_y, screenmax_x
     toggle_unit = 0
     match(pressed_key):
         case pygame.K_1:
             toggle_unit = 1
         case pygame.K_2:
             toggle_unit = 2
+        case pygame.K_w:
+            if offset_y <= -50:
+                offset_y += 50
+                print(f"y offset now {offset_y}")
+        case pygame.K_s:
+            if offset_y >= (-1 * screenmax_y) + 50:
+                offset_y -= 50
+                print(f"y offset now {offset_y}")
+        case pygame.K_a:
+            if offset_x <= -50:
+                offset_x += 50
+                print(f"x offset now {offset_x}")
+        case pygame.K_d:
+            if offset_x >= (-1 * screenmax_x) + 50:
+                offset_x -= 50
+                print(f"x offset now {offset_x}")
     for unit in PlayerGroup:
         if unit.unit_number == toggle_unit:
             toggle_selection(unit)
@@ -33,9 +49,9 @@ def toggle_selection(unit):
         unit.selected = True
     
 def mouse_interpreter(clicked_position, clicked_button):
-    global SelectionGroup
+    global SelectionGroup, offset_x, offset_y
     if clicked_button[0] or clicked_button[1]:
-        go_here = SelectionCursor(clicked_position[0], clicked_position[1])
+        go_here = SelectionCursor(clicked_position[0] + (-1 * offset_x), clicked_position[1] + (-1 * offset_y))
         for unit in SelectionGroup:
             unit.destination = go_here
             print(f"setting {unit.name} destination to {unit.destination.position}")
@@ -78,6 +94,13 @@ class SelectionCursor(RootObject): #selection cursor. should move these to other
 def battle_mode(screen):
     clock = pygame.time.Clock()
     dt = 0
+    
+    global offset_x, offset_y, screenmax_x, screenmax_y
+    offset_x = 0
+    offset_y = 0
+    screenmax_x = SCREEN_WIDTH * 3
+    screenmax_y = SCREEN_HEIGHT * 3
+    playable_area = pygame.surface.Surface((screenmax_x, screenmax_y))
 
     #create the map, populate with units
 
@@ -103,8 +126,8 @@ def battle_mode(screen):
     Player = PlayerRobot(250, 300, "john character", 1)
     Player2 = PlayerRobot(250, 650, "jane character", 2)
     
-    Spawner = EnemySpawner(900, 500)
-    Spawner2 = EnemySpawner(900, 200)
+    Spawner = EnemySpawner(1500, 500)
+    Spawner2 = EnemySpawner(1800, 900)
 
     action_waiting = False
     kbd_waiting = False
@@ -150,7 +173,7 @@ def battle_mode(screen):
                     action_waiting = False
                     print("some other action took place?")
 
-        pygame.Surface.fill(screen, "black")
+        pygame.Surface.fill(playable_area, "purple")
 
         for item in loop_updatable:
             if item in PlayerGroup:
@@ -161,8 +184,9 @@ def battle_mode(screen):
                 item.update(dt)
         
         for item in loop_drawable:
-            item.rect = item.draw(screen)    
+            item.rect = item.draw(playable_area)    
 
-
+        drawable_rect = pygame.rect.Rect(offset_x, offset_y, SCREEN_WIDTH, SCREEN_HEIGHT)
+        pygame.surface.Surface.blit(screen, playable_area, drawable_rect)
         pygame.display.flip()
         dt = clock.tick(60)/1000      
