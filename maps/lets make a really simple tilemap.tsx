cf97d3727cc2700647f0c1db11b2640f2b0f416a
themuffinlord <tcgiant@gmail.com>
2025-05-13 18:07:25 -0400

    map gen part 1.25

---
+++ maps/lets make a really simple tilemap.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="lets make a really simple tilemap" tilewidth="64" tileheight="64" tilecount="16" columns="8">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="lets make a really simple tilemap.png" width="512" height="128"/>
+ <tile id="0" type="grass1"/>
+ <tile id="1" type="grass2"/>
+ <tile id="2" type="water1"/>
+ <tile id="3" type="water2"/>
+ <tile id="4" type="rock1"/>
+ <tile id="5" type="dirt1"/>
+ <tile id="7" type="lure"/>
+ <tile id="8" type="grass3"/>
+ <tile id="9" type="grass4"/>
+ <tile id="10" type="water3"/>
+ <tile id="11" type="water4"/>
+ <tile id="12" type="rock2"/>
+ <tile id="13" type="dirt2"/>
+ <tile id="15" type="spawnpoint"/>
+</tileset>
---
+++ maps/lets make a tilemap.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="lets make a tilemap" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="lets make a tilemap.aseprite" width="256" height="64"/>
+ <tile id="0" type="grass" probability="0.25"/>
+ <tile id="1" type="grass" probability="0.25"/>
+ <tile id="2" type="water1"/>
+ <tile id="3" type="water2"/>
+ <tile id="4" type="goop1"/>
+ <tile id="5" type="rock"/>
+ <tile id="8" type="grass" probability="0.25"/>
+ <tile id="9" type="grass" probability="0.25"/>
+ <tile id="10" type="water3"/>
+ <tile id="11" type="water4"/>
+ <tile id="12" type="goop2"/>
+ <tile id="13" type="tree"/>
+</tileset>
